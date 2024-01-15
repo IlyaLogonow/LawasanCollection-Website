4c652c0763e97737f8e5304ed7ae0b6b5ffc5dcb
ninjoix <ninjoix@gmail.com>
2024-01-15 18:50:14 +0300

    Made a hero-slider block, start making a Payday Deals block

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
